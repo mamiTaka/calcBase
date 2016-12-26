bb8dd9b5b5a00c629a658546af7cfb581ce7fdc9
高橋真未 <takahashimami@takahashimami-no-macbook-air.local>
2016-12-27 07:49:38 +0900

    Add　基本の電卓機能
    基本の電卓機能を作成

--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -9,7 +9,124 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var firstNumber = ""
+    var kigou = ""
+    var secondNumber = ""
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func oneButtonPushdown(_ sender: UIButton) {
+        resultLabel.text = resultLabel.text! + "1"
+    }
+    
+    @IBAction func twoButtonPushdown(_ sender: UIButton) {
+        resultLabel.text = resultLabel.text! + "2"
+    }
+    
+    @IBAction func thereeButtonPushdown(_ sender: UIButton) {
+        resultLabel.text = resultLabel.text! + "3"
+    }
+    @IBAction func fourButtonPushdown(_ sender: UIButton) {
+        resultLabel.text = resultLabel.text! + "4"
+    }
+    @IBAction func fiveButtonPushdown(_ sender: UIButton) {
+        resultLabel.text = resultLabel.text! + "5"
+    }
+    @IBAction func sixButtonPushdown(_ sender: UIButton) {
+        resultLabel.text = resultLabel.text! + "6"
+    }
+    @IBAction func sevenButtonPushdown(_ sender: UIButton) {
+        resultLabel.text = resultLabel.text! + "7"
+    }
+    
+    @IBAction func eightButtonPushdown(_ sender: UIButton) {
+        resultLabel.text = resultLabel.text! + "8"
+    }
+    
+    @IBAction func nineButtonPushdown(_ sender: UIButton) {
+        resultLabel.text = resultLabel.text! + "9"
+    }
+    
+    @IBAction func zeroButtonPushdown(_ sender: UIButton) {
+        resultLabel.text = resultLabel.text! + "0"
+    }
+    
+    @IBAction func PointButtonPushdown(_ sender: UIButton) {
+        resultLabel.text = resultLabel.text! + "."
+    }
+    
+    @IBAction func pulasButtonPushdown(_ sender: UIButton) {
+        kigou = "+"
+        firstNumber = resultLabel.text!
+        resultLabel.text = ""
+    }
+    
+    @IBAction func minusPushdown(_ sender: UIButton) {
+        kigou = "-"
+        firstNumber = resultLabel.text!
+        resultLabel.text = ""
+    }
+    
+    @IBAction func PlayButtonPushdown(_ sender: UIButton) {
+        kigou = "×"
+        firstNumber = resultLabel.text!
+        resultLabel.text = ""
+    }
+    
+    @IBAction func BreakButtonPushdown(_ sender: UIButton) {
+        kigou = "÷"
+        firstNumber = resultLabel.text!
+        resultLabel.text = ""
+    }
+    
+    
+    @IBAction func equalButtonPushdown(_ sender: UIButton) {
+        secondNumber = resultLabel.text!
+        var x:Double = NSString(string: firstNumber).doubleValue
+        var y:Double = NSString(string: secondNumber).doubleValue
+        var z:Double = 0
+        
+//        if kigou == "+"  {
+//        z = x + y
+//            var answer: String = NSString(format: "%.1f", z) as String
+//            resultLabel.text = answer
+//        }else if kigou == "-" {
+//            z = x - y
+//            var answer: String = NSString(format: "%.1f", z) as String
+//            resultLabel.text = answer
+//        }else if kigou == "×" {
+//            z = x * y
+//            var answer: String = NSString(format: "%.1f", z) as String
+//            resultLabel.text = answer
+//        }else {
+//            z = x / y
+//            var answer: String = NSString(format: "%.1f", z) as String
+//            resultLabel.text = answer
+//        
+//        }
+//    }
 
+        switch kigou {
+        case "+":
+            z = x + y
+            
+        case "-":
+            z = x - y
+            
+        case "×":
+            z = x * y
+
+        case "÷":
+            z = x / y
+        
+        default: break
+        }
+        
+        var answer: String = NSString(format: "%.1f", z) as String
+        resultLabel.text = answer
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +136,4 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
